ff5179caadecc653b011baee1f1e0bc1d7aab85c
Juraj Piovar <jpiovar@sk.ibm.com>
2018-08-01 21:17:38 +0200

    put2 standard way vue

---
+++ put2/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <input v-model="msg">
+    <p>prop: {{propMessage}}</p>
+    <p>msg: {{msg}}</p>
+    <p>helloMsg: {{helloMsg}}</p>
+    <p>computed msg: {{computedMsg}}</p>
+    <button @click="greet">Greet</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      propMessage: 'propMessage ok',
+      helloMsg: 'helloMsg ok',
+      computedMsg: 'computedMsg ok',
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    greet () {
+      console.log('greet ok')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
